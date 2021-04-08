cd05be1f2acea9a3070d73293845b6f3dd4cc73a
Janeé Mays <janee.elise@gmail.com>
2021-04-07 20:16:05 -0400

    added listener

--- server.js
+++ server.js
@@ -2,3 +2,12 @@ const express = require('express');
 
 const app = express();
 const PORT = 8080;
+
+// Routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join)
+})
+
+
+// Listener
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
