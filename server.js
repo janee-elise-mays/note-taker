5f99b5af03de5003e8f744562813a71301c7c986
Janeé Mays <janee.elise@gmail.com>
2021-04-10 16:42:39 -0400

    finished post api

--- server.js
+++ server.js
@@ -1,23 +1,45 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const uuid = require('uuid').v4;
 
 const app = express();
 const PORT = 8080;
 
 // Routes
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json());
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/assets/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/assets/notes.html')));
 
-module.exports = (app) => {
-    // Display all notes
-    app.get('/api/notes', (req, res) => res.json(noteInput));
+// Display all notes
+app.get('/api/notes', (req, res) => {
+    let noteInput = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf-8');
+    noteInput = JSON.parse(noteInput);
+    res.json(noteInput)
+});
 
-    // Create new notes - takes JSON dataInput
-    app.post('/api/notes', (req, res) => {
-        noteInput.push(req.body);
-    });
-};
+// Create new notes - takes JSON dataInput
+app.post('/api/notes', (req, res) => {
+    let noteInput = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf-8');
+    noteInput = JSON.parse(noteInput);
+    req.body.id = uuid();
+    noteInput.push(req.body);
+    noteInput = JSON.stringify(noteInput);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), noteInput);
+    console.log(req.body);
+    res.json(noteInput);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const noteID = req.params.id;
+    let noteInput = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf-8');
+    noteInput = JSON.parse(noteInput);
+    // remove the note from the noteinput (the one with the matching id)  filter/splice function
+    // write the notes array back to the file (fs.writeFileSync) similar for delete
+    res.json(noteInput);
+});
+
 // Listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
