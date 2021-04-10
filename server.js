b87ba103b1105d4ada110ed9a1354eb6d8ea2e21
Janeé Mays <janee.elise@gmail.com>
2021-04-10 15:37:06 -0400

    added get apis

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/assets/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/assets/notes.html')));
 
+module.exports = (app) => {
+    // Display all notes
+    app.get('/api/notes', (req, res) => res.json(noteInput));
 
-
+    // Create new notes - takes JSON dataInput
+    app.post('/api/notes', (req, res) => {
+        noteInput.push(req.body);
+    });
+};
 // Listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
