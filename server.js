7026c727ac670b3c7bf8350b83725475889eca49
Janeé Mays <janee.elise@gmail.com>
2021-04-09 20:25:39 -0400

    added .gitignore

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 8080;
 
 // Routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/notes.html')));
 
 
 // Listener
